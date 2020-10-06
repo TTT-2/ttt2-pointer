30710c9d55b96ce316256cc46c683370d9c87712
ThePlatinumGhost <36534723+ThePlatinumGhost@users.noreply.github.com>
2020-10-06 13:54:09 +0200

    fix

--- lua/terrortown/lang/italian/pointer.lua
+++ lua/terrortown/lang/italian/pointer.lua
@@ -5,6 +5,6 @@ L["ttt2_pointer_timeout"] = "Devi aspettare {time} secondi per aggiungere un nuo
 L["ttt2_pointer_distance"] = "Distanza: {distance}"
 L["ttt2_pointer_time"] = "Tempo rimanente: {time}"
 
-L["header_bindings_pointer"] = "TTT2 3D Puntatore"
+L["header_bindings_pointer"] = "TTT2 Puntatore 3D"
 L["label_bind_pointer_team"] = "Puntatore Team"
 L["label_bind_pointer_global"] = "Puntatore Globale"
