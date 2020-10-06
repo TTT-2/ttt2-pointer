c08f6a2a429da3b6668f1e25d3255b2b18586463
Blueberryy <36592509+Blueberryy@users.noreply.github.com>
2020-10-06 16:25:57 +0500

    Russian translation (#9)

---
+++ lua/terrortown/lang/russian/pointer.lua
@@ -0,0 +1,6 @@
+L = LANG.GetLanguageTableReference("Русский")
+
+L["ttt2_pointer_new"] = "{playername} добавил новый 3D-указатель!"
+L["ttt2_pointer_timeout"] = "Вы должны подождать {time} сек., чтобы добавить новый указатель!"
+L["ttt2_pointer_distance"] = "Расстояние: {distance}"
+L["ttt2_pointer_time"] = "Времени осталось: {time}"
