2817a0dfc5cfbe3616424e17fd6468c0850dbe1d
ThePlatinumGhost <36534723+ThePlatinumGhost@users.noreply.github.com>
2020-10-06 13:51:05 +0200

    Italian translation

---
+++ lua/terrortown/lang/italian/pointer.lua
@@ -0,0 +1,10 @@
+L = LANG.GetLanguageTableReference("italiano")
+
+L["ttt2_pointer_new"] = "{playername} Ha aggiunto un nuovo puntatore 3D!"
+L["ttt2_pointer_timeout"] = "Devi aspettare {time} secondi per aggiungere un nuovo puntatore!"
+L["ttt2_pointer_distance"] = "Distanza: {distance}"
+L["ttt2_pointer_time"] = "Tempo rimanente: {time}"
+
+L["header_bindings_pointer"] = "TTT2 3D Puntatore"
+L["label_bind_pointer_team"] = "Puntatore Team"
+L["label_bind_pointer_global"] = "Puntatore Globale"
