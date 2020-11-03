a7f744d75f1fd52356b76350ab05f257bcde2ede
MisterClems <73431903+MisterClems@users.noreply.github.com>
2020-11-03 09:31:17 +0100

    Create pointer.lua (#11)
    french translation : pointer

---
+++ lua/terrortown/lang/french/pointer.lua
@@ -0,0 +1,10 @@
+L = LANG.GetLanguageTableReference("french")
+
+L["ttt2_pointer_new"] = "{playername} a ajouté un nouveau pointeur 3D!"
+L["ttt2_pointer_timeout"] = "Vous devez attendre {time} seconde(s) pour ajouter un nouveau pointeur!"
+L["ttt2_pointer_distance"] = "Distance: {distance}"
+L["ttt2_pointer_time"] = "Temps restant: {time}"
+
+L["header_bindings_pointer"] = "TTT2 Pointeur 3D"
+L["label_bind_pointer_team"] = "Team Pointeur"
+L["label_bind_pointer_global"] = "Pointeur Global"
